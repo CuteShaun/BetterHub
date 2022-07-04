40f532f0ee8745751a2f2c0fc962c875d8d41940
atsapko-at-999781611676 <user@tsapko-macbook-pro.local>
2022-07-05 02:02:39 +0300

    BetterHub: add constants file

---
+++ src/contstants.js
@@ -0,0 +1,6 @@
+export const API_KEY = 'dada';
+export const DEFAULT_SORT = 'stars';
+export const DEFAULT_ORDER = 'asc';
+export const DEFAULT_PER_PAGE = 30;
+export const DEFAULT_PAGE = 1;
+export const BASE_URL = "https://github.com";
