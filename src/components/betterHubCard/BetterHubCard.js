d868a2e8cc5de4c516892249156248841e1903bb
atsapko-at-999781611676 <user@tsapko-macbook-pro.local>
2022-07-05 02:12:50 +0300

    BetterHub: add betterHubCard component

---
+++ src/components/betterHubCard/BetterHubCard.js
@@ -0,0 +1,69 @@
+import React from "react";
+import { ReactComponent as StarIcon } from "./star.svg";
+import { getProgrammingLanguageColor } from "../../helpers";
+import { BASE_URL } from "../../contstants";
+import "./BetterHubCard.scss";
+
+export const BetterHubCard = ({
+  title = "",
+  desc = "",
+  stars = "",
+  license = "",
+  lang = "",
+  topics = "",
+  url = "",
+  issues = "",
+} = {}) => {
+  const renderCardTopics = () => {
+    return topics.map((topic, index) => {
+      return (
+        <a className="card__topics-item" href={`${BASE_URL}/topics/${topic}`} key={index}>
+          {topic}
+        </a>
+      );
+    });
+  };
+
+  const renderDescription = () => {
+    if (desc && desc.length > 300) {
+      return `${desc.substring(0, 300)}...`;
+    }
+
+    return desc;
+  };
+
+  return (
+    <div className="card">
+      <h2 className="card__title">
+        <a className="card__link" href={url}>
+          {title}
+        </a>
+      </h2>
+      <div className="card__description">{renderDescription()}</div>
+      <div className="card__topics">{renderCardTopics()}</div>
+
+      <section className="card__footer">
+        <a href={`${BASE_URL}/${title}/stargazers`} className="card__stars card__footer-item">
+          <StarIcon />
+          {stars}
+        </a>
+
+        {lang && (
+          <div className="card__language card__footer-item">
+            <span
+              className="card__language-color"
+              style={{ backgroundColor: getProgrammingLanguageColor(lang?.toLowerCase()?.trim()) }}
+            ></span>
+            <span className="card__language-text">{lang}</span>
+          </div>
+        )}
+
+        {license && <div className="card__license card__footer-item">{license}</div>}
+
+        {issues && (
+          <div className="card__issues card__footer-item">{`${issues} issues need help`}</div>
+        )}
+      </section>
+    </div>
+  );
+};
